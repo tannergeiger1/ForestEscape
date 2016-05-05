d6556cddfcc98570e613046e68f8850a61495d90
emmab11 <BrownE0154@sduhsd.lan>
2016-05-05 08:15:26 -0700

    Machine/Zombie/Wind Sounds

---
+++ Unity3DExample/Assets/Scripts/WindSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindSound : MonoBehaviour {
+
+
+	private AudioSource source;		// Reference to the animator component.
+	public AudioClip WSound;
+	public float vol = 0.5f;
+
+	void Awake(){
+		source = GetComponent<AudioSource>();
+
+	}
+
+	void OnTriggerEnter (Collider other){
+
+		source.PlayOneShot(WSound,vol);
+	}
+
+	void OnTriggerExit (Collider other){
+
+	}
+}
