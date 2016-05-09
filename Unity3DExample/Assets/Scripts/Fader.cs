8b292cd26ca8658bf539abf486fd2ac50462ab51
tannergeiger1 <geiger.tanner12@gmail.com>
2016-05-09 09:26:40 -0700

    level transition

---
+++ Unity3DExample/Assets/Scripts/Fader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fader : MonoBehaviour {
+
+	public bool start = false;
+	public float fadeDamp = 0.0f;
+	public string fadeScene;
+	public float alpha = 0.0f;
+	public Color fadeColor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		if (!start)
+			return;
+		GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, GradientAlphaKey);
+
+		Texture2D myTex;
+		myTex = new Texture2D (1, 1);
+		myTex.SetPixel (0, 0, fadeColor);
+		myTex.Apply ();
+
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), myTex);
+		alpha = Mathf.Lerp (alpha,1.1f)
+	
+	}
+}
---
+++ Unity3DExample/Assets/Scripts/Test.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+	public string scene;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+	if (GUI.Button (new Rect (0, 0, 100, 30), "Start")) {
+			Initiate.Fade (scene,Color.green,0.5f);
+		}
+	}
+}
