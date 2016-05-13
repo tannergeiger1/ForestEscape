830a82f37ef768d41e5e1e37ebf6f74916ea3167
emmab11 <BrownE0154@sduhsd.lan>
2016-05-13 08:06:14 -0700

    Music

---
+++ Unity3DExample/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour {
+
+	void OnMouseDown() {
+		Play();
+	}
+	void Play() {
+		Application.LoadLevel("Scene_1");
+	}
+}
