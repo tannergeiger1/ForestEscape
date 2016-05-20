19b0bc18e4f02fc906129662ea6c64bbdc444562
emmab11 <BrownE0154@sduhsd.lan>
2016-05-20 08:28:07 -0700

    gvnch

---
+++ Unity3DExample/Assets/Scripts/UIManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class UIManager : MonoBehaviour
+
+{                         // Reference to the animator component.
+	GameObject[] pauseObjects;
+	
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 1;
+		pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+		hidePaused();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetMouseButton (0)) {
+			pauseControl();
+		}
+
+		//uses the p button to pause and unpause the game
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			if(Time.timeScale == 1)
+			
+			{
+				Time.timeScale = 0;
+
+				showPaused();
+			} else if (Time.timeScale == 0){
+				Debug.Log ("high");
+				Time.timeScale = 1;
+				hidePaused();
+			}
+		}
+	}
+	//Reloads the Level
+	public void Reload(){
+		Application.LoadLevel(Application.loadedLevel);
+	}
+	
+	//controls the pausing of the scene
+	public void pauseControl(){
+
+		Debug.Log ("press");
+
+		if(Time.timeScale == 1)
+		{
+			Time.timeScale = 0;
+			showPaused();
+		} else if (Time.timeScale == 0){
+			Time.timeScale = 1;
+			hidePaused();
+		}
+	}
+
+	//shows objects with ShowOnPause tag
+	public void showPaused(){
+		foreach(GameObject g in pauseObjects){
+			g.SetActive(true);
+		}
+	}
+	
+	//hides objects with ShowOnPause tag
+	public void hidePaused(){
+		foreach(GameObject g in pauseObjects){
+			g.SetActive(false);
+		}
+	}
+	
+	//loads inputted level
+	public void LoadLevel(string level){
+		Application.LoadLevel (level);
+	}
+	}
