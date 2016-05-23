f00cac26c00bd4a3fd2ac999f1da6e2a4edff800
tannergeiger1 <geiger.tanner12@gmail.com>
2016-05-23 08:32:51 -0700

    working on zombie

--- Unity3DExample/Assets/Scripts/Follow.cs
+++ Unity3DExample/Assets/Scripts/Follow.cs
@@ -4,12 +4,7 @@ using System.Collections;
 public class Follow : MonoBehaviour {
 	
 	Transform tr_Player;
-<<<<<<< HEAD
-	float f_RotSpeed=3.0f,f_MoveSpeed = .02f;
-=======
 	float f_RotSpeed=3.0f,f_MoveSpeed = 1.0f;
->>>>>>> refs/remotes/origin/master
-	
 	// Use this for initialization
 	void Start () {
 		
--- Unity3DExample/Assets/Scripts/UIManager.cs
+++ Unity3DExample/Assets/Scripts/UIManager.cs
@@ -19,11 +19,7 @@ public class UIManager : MonoBehaviour
 
 
 		if (Input.GetMouseButton (0)) {
-<<<<<<< HEAD
 			pauseControl();
-=======
-
->>>>>>> refs/remotes/origin/master
 		}
 
 		//uses the p button to pause and unpause the game
@@ -49,12 +45,8 @@ public class UIManager : MonoBehaviour
 	
 	//controls the pausing of the scene
 	public void pauseControl(){
-<<<<<<< HEAD
 
 		Debug.Log ("press");
-
-=======
->>>>>>> refs/remotes/origin/master
 		if(Time.timeScale == 1)
 		{
 			Time.timeScale = 0;
