a291d41158b8d7c176b8d2bc3fa96273ade1ffd2
emmab11 <BrownE0154@sduhsd.lan>
2016-05-25 10:09:39 -0700

    525

--- Unity3DExample/Assets/Scripts/UIManager.cs
+++ Unity3DExample/Assets/Scripts/UIManager.cs
@@ -1,80 +1,83 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
 
 
 public class UIManager : MonoBehaviour
-
+	
 {                         // Reference to the animator component.
 	GameObject[] pauseObjects;
-	
+	public GameObject player;
+	private CharacterController controller;
 	// Use this for initialization
 	void Start () {
 		Time.timeScale = 1;
 		pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
 		hidePaused();
+		controller = player.GetComponent<CharacterController> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-
+		
+		
 		if (Input.GetMouseButton (0)) {
-<<<<<<< HEAD
-
 			pauseControl();
-
-=======
-			pauseControl();
->>>>>>> refs/remotes/origin/master
 		}
-
+		
 		//uses the p button to pause and unpause the game
 		if(Input.GetKeyDown(KeyCode.P))
-		{
-			if(Time.timeScale == 1)
 			
+		{
+			
+			if(Time.timeScale == 1)
+				
 			{
 				Time.timeScale = 0;
-
+				GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = false;
+				
 				showPaused();
 			} else if (Time.timeScale == 0){
 				Debug.Log ("high");
 				Time.timeScale = 1;
 				hidePaused();
+				GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = true;
+				
 			}
 		}
 	}
 	//Reloads the Level
 	public void Reload(){
+		DontDestroyOnLoad (controller);
 		Application.LoadLevel(Application.loadedLevel);
 	}
 	
 	//controls the pausing of the scene
 	public void pauseControl(){
-
-<<<<<<< HEAD
-
+		
 		Debug.Log ("press");
 		
-=======
-		Debug.Log ("press");
->>>>>>> refs/remotes/origin/master
 		if(Time.timeScale == 1)
 		{
 			Time.timeScale = 0;
+			GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = false;
 			showPaused();
 		} else if (Time.timeScale == 0){
 			Time.timeScale = 1;
 			hidePaused();
+			GameObject.Find("Player").GetComponent<FirstPersonController>().enabled = true;
 		}
 	}
-
+	
+	
 	//shows objects with ShowOnPause tag
 	public void showPaused(){
+		DontDestroyOnLoad (controller);
 		foreach(GameObject g in pauseObjects){
 			g.SetActive(true);
 		}
 	}
+	
 	
 	//hides objects with ShowOnPause tag
 	public void hidePaused(){
@@ -85,6 +88,8 @@ public class UIManager : MonoBehaviour
 	
 	//loads inputted level
 	public void LoadLevel(string level){
+		DontDestroyOnLoad (controller);
 		Application.LoadLevel (level);
 	}
-	}
+}
+
