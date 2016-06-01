5e277204d8d0b280824aff1f26ec742083f8dae2
tannergeiger1 <geiger.tanner12@gmail.com>
2016-06-01 10:38:40 -0700

    htsrhd

--- Unity3DExample/Assets/Scripts/MoveScene.js
+++ Unity3DExample/Assets/Scripts/MoveScene.js
@@ -7,7 +7,11 @@ function OnTriggerEnter (Col : Collider)
 	if (Col.CompareTag("Player"))
 	{
 <<<<<<< HEAD
+<<<<<<< HEAD
 		Application.LoadLevel (2);	
+=======
+	Application.LoadLevel (newScene);
+>>>>>>> parent of 05631fd... fjehfiuewl
 =======
 	Application.LoadLevel (newScene);
 >>>>>>> parent of 05631fd... fjehfiuewl
