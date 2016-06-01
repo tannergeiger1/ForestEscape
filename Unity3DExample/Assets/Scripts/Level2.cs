e11942f51aa454c31aba19acdc9c98769f8430b7
emmab11 <BrownE0154@sduhsd.lan>
2016-06-01 10:23:19 -0700

    33

---
+++ Unity3DExample/Assets/Scripts/Level2.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level2 : MonoBehaviour {
+
+	void OnCollisionEnter() {
+		Collider();
+	}
+	void Collider() {
+		Application.LoadLevel("Level2");
+	}
+}
